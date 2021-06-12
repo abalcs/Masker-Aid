f4cc6eabb5fd032049dcc7892f46a6d124f75c82
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-06-12 14:19:57 -0400

    got results.hbs page connected

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes')
+const resultRoutes = require('./resultRoutes')
 
 router.use('/api', apiRoutes);
+router.use('/results', resultRoutes);
 // router.use('/', homeRoutes);
-// router.use('/dashboard', dashboardRoutes)
 
 router.get('/', (req, res) => {
   res.render('dashboard');
 });
+
+router.get('/results', (req, res) => {
+    res.render('results');
+  });
 
 router.use((req, res) => {
   res.status(404).end();
