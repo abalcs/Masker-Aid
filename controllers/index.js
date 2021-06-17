a785527eb26b8b25b069e43d28cb3c56b5a5353f
Erica Fagioli <efagioli01@gmail.com>
2021-06-17 19:27:00 -0400

    added routes to index

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes')
+const contactRoutes = require('./contactRoutes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/contact', contactRoutes)
 
 module.exports = router;
