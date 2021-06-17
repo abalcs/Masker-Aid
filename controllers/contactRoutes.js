47f984f971abe4b0084d69fdcd3a7d08839a21d4
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-06-17 19:45:54 -0400

    ya no idea here goes nothing

--- controllers/contactRoutes.js
+++ controllers/contactRoutes.js
@@ -1,21 +1,13 @@
 const router = require('express').Router();
 
-
 // Home page route.
 router.get('/', function (req, res) {
     res.send('contact page');
-  })
+})
 
-// About page route.
+// Contact page route.
 router.get('/contact', function (req, res) {
     res.send('About us');
-  })
+})
   
-
-
-
-
-
-
-
 module.exports = router;
