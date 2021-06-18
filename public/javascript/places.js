55b734e6461fac22d494df339e1a9ce3af545c69
James Clark <clarkey95@hotmail.com>
2021-06-18 17:52:18 -0400

    modified the dynamically created place result

--- public/javascript/places.js
+++ public/javascript/places.js
@@ -16,10 +16,17 @@ function initAutoComplete() {
       console.log('selected place', place);
 
       let card = $('<div>').addClass("card");
-      let cardBody = $('<div>').addClass("card-body");
-      let cardTitle = $('<h3>').addClass('card-title').text(place.name);
 
-      $('body').append(card.append(place.name))
+      // Append Place information dynamically
+
+      $('body').append(card.append(`<div class="card-body w-50">${place.name} <br>${place.adr_address}<br><a href="${place.url}">View on Maps</a></div><label for="choices">Mask Options:</label>
+
+      <select name="choices" id="options">
+        <option value="mask">Mask Required</option>
+        <option value="no-mask">No Mask Required</option>
+        <option value="passport">Proof of Vaccine</option>
+      </select>
+      <button class="" id="save-btn">Save</button>`))
 
       fetch(`/api/places/${place.place_id}`, {
       }).then((resp) => resp.json()).then((data) => {
