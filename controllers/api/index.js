99d4f8ba6c3240833c0121d5c5d781759d2434d5
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-06-19 11:46:26 -0400

    saving places js and routes updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const placesRoutes = require('./placesRoutes');
+
 
 router.use('/users', userRoutes);
+router.use('/places', placesRoutes);
 
 module.exports = router;
